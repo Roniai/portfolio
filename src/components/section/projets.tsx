196a4fe8c2a3eb8a9cc0a55c90c2056ecdc89f53
Roniai <valitera.fehizoro@gmail.com>
2025-11-07 11:30:48 +0300

    feat(projects): responsive deisgn of projects section

--- src/components/section/projets.tsx
+++ src/components/section/projets.tsx
@@ -7,33 +7,25 @@ export const Projects = async () => {
   const t = await getTranslations("ProjectsPage");
   const projectsDescription = t.raw("projectsDescription");
 
-  const chunkArray = (arr: any[], size: number) => {
-    return arr.reduce(
-      (acc, _, i) => (i % size ? acc : [...acc, arr.slice(i, i + size)]),
-      []
-    );
-  };
-
-  const projectChunks = chunkArray(projectsData, 3);
-
   return (
-    <section id="projects" className="font-primary mx-20 scroll-mt-20">
+    <section
+      id="projects"
+      className="font-primary scroll-mt-20 px-4 sm:px-6 xl:px-20"
+    >
       <SectionTitle title={t("projectTitle")} />
       <div className="space-y-10">
-        {projectChunks.map((chunk: any[], rowIndex: number) => (
-          <div key={rowIndex} className="grid grid-cols-3 gap-10">
-            {chunk.map((project, i) => (
-              <ProjectCard
-                key={i}
-                id={rowIndex * 3 + i}
-                title={project.title}
-                description={projectsDescription[rowIndex * 3 + i]}
-                image={project.imagePath}
-                stacks={project.stacks}
-              />
-            ))}
-          </div>
-        ))}
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+          {projectsData.map((project, i) => (
+            <ProjectCard
+              key={i}
+              id={i}
+              title={project.title}
+              description={projectsDescription[i]}
+              image={project.imagePath}
+              stacks={project.stacks}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
