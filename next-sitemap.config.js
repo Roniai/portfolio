9052ce723d623101f5127706bf3ee5a1c14b641e
Roniai <valitera.fehizoro@gmail.com>
2026-03-13 15:12:53 +0300

    fix: update config sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://fehizoroniaina-indrafo.vercel.app",
-  generateRobotsTxt: true, // Génère aussi le robots.txt automatiquement si tu veux
-  sitemapSize: 5000,
+  siteUrl: 'https://fehizoroniaina-indrafo.vercel.app',
+  generateRobotsTxt: true, // génère robots.txt automatiquement
+  changefreq: 'weekly',
+  priority: 1.0,
+
+  // Si tu veux, tu peux déclarer la seule page
+  additionalPaths: async (config) => [
+    { loc: '/', lastmod: new Date().toISOString() }
+  ],
 };
